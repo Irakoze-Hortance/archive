5fb106aafea88dd1a969aab71daba13a6172c6e9
Irakoze Hortance <hortanceirakoze@gmail.com>
2021-10-26 09:16:12 -0700

    author model

---
+++ Models/authorModel.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose')
+const Joi=require('joi')
+const Schema = mongoose.Schema
+
+const Author=new Schema({
+    name:{
+        type: String,
+        required: true,
+    },
+
+    post_id:{
+        type: String,
+        required:true,
+
+    },
+    email:{
+        type:String,
+        required:true,
+    },
+    password:{
+        type: String,
+        required:true,
+    }
+})
+
+function validateAuthor(Author){
+    const JoiSChema=Joi.object({
+     name:Joi.string().required(),
+     post_id:Joi.string().required(),
+     email:Joi.string().required(),
+     password:Joi.string().required(),
+    }).options({abortEarly:false});
+return JoiSChema.validate(Author)
+}
+module.exports.Author = mongoose.model('Author', Author)
+module.exports.validateAuthor = validateAuthor
