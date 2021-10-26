ea4aa14ee7b605f036c5c402e6975dbbfda8a9db
Irakoze Hortance <hortanceirakoze@gmail.com>
2021-10-26 09:25:46 -0700

    added config

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express=require('express');
 const bodyParser=require('body-parser');
 const cors=require('cors')
 
-require('./DB/db')
+require('./config/db')
 const app=express()
 const port=8000
 // const flightRouter=require('./routes/flightRoute')
