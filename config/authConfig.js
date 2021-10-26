bd87417331a0a431b94aefd7863124b6437d3bb0
Irakoze Hortance <hortanceirakoze@gmail.com>
2021-10-26 09:42:39 -0700

    auth and db configurations

---
+++ config/authConfig.js
@@ -0,0 +1,3 @@
+module.exports={
+    secret:"hortance-secret-key"
+}
--- config/db.js
+++ config/db.js
@@ -1,16 +1,13 @@
-const mongoose=require('mongoose')
 
-mongoose.connect('localhost:27017/saintly',{
-    useNewUrlParser:true,
-    useUnifiedTopology:true
-})
-.then(()=>{
-    console.log("Db connected!")
-})
-.catch(e=>{
-    console.error('Connection error',e.message)
-})
 
-const db=mongoose.connection
-
-module.exports=db
+module.exports={
+    HOST:"localhost",
+    PORT:27017,
+    DB:"saintly"
+    .then(()=>{
+        console.log("Db connected!")
+    })
+    .catch(e=>{
+        console.error('Connection error',e.message)
+    })
+}
